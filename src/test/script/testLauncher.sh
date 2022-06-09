18fce308c44b0869db3565d0cf8d601276b55f07
Antoine Klein <antoine.klein3@grenoble-inp.org>
2022-06-09 09:09:29 +0000

    Resolve "test decompilation"

--- src/test/script/testLauncher.sh
+++ src/test/script/testLauncher.sh
@@ -105,25 +105,49 @@ fi
 if [ -d  "${root}/codegen/invalid/${feature}" ]; then
     cd "${root}/codegen/invalid/${feature}"
     echo "### TEST: $(pwd) ###"
-    rm -f *.res *.ass || exit 1
+    rm -f *.res *.ass *_decompiled.deca || exit 1
     for f in *.deca ; do
         file="${f%.deca}"
         ((nbtests++))
+        # if $2 is set, we want to test the codegen with the given compiler
+        if [ "$2" != "" ]; then
+            decac -p "${f}" > "${file}_decompiled.deca"
+        fi
         if [ -f "${file}.in" ]; then
+            if [ "$2" != "" ]; then
+            decac "${file}_decompiled.deca" && (cat "${file}.in" | ima "${file}_decompiled.ass" > "${file}_decompiled.res")
+            fi
             decac "${f}" && (cat "${file}.in" | ima "${file}.ass" > "${file}.res")
         else
+            if [ "$2" != "" ]; then
+                decac "${file}_decompiled.deca" && (ima "${file}_decompiled.ass" > "${file}_decompiled.res")
+            fi
             decac "${f}" && (ima "${file}.ass" > "${file}.res")
         fi
         if [ -f "${file}.res" ]; then
-    	if diff -q "${file}.res" "${file}.expected" > /dev/null ; then
-    	    echo "--- ${file}: PASSED ---"
-    	    ((nbpassed++))
-    	else
-    	    echo "--- ${file}: FAILED ---"
-    	    diff "${file}.expected" "${file}.res" 
-    	fi
+    	    if diff -q "${file}.res" "${file}.expected" > /dev/null ; then
+                if [ "$2" != "" ]; then
+                    if [ -f "${file}_decompiled.res" ]; then
+    	                if diff -q "${file}_decompiled.res" "${file}.expected" > /dev/null ; then
+                            echo "--- ${file}: PASSED ---"
+    	                    ((nbpassed++))
+                        else
+                            echo "--- ${file}: FAILED ---"
+                            diff "${file}.expected" "${file}_decompiled.res"
+                        fi
+                    else
+                        echo "--- ${file}: KO ---"
+                    fi
+                else
+    	            echo "--- ${file}: PASSED ---"
+    	            ((nbpassed++))
+                fi
+    	    else
+    	        echo "--- ${file}: FAILED ---"
+    	        diff "${file}.expected" "${file}.res" 
+    	    fi
         else
-    	echo "--- ${file}: KO ---"
+    	    echo "--- ${file}: KO ---"
         fi
         echo
     done
@@ -132,25 +156,49 @@ fi
 if [ -d  "${root}/codegen/valid/${feature}" ]; then
     cd "${root}/codegen/valid/${feature}"
     echo "### TEST: $(pwd) ###"
-    rm -f *.res *.ass || exit 1
+    rm -f *.res *.ass *_decompiled.deca|| exit 1
     for f in *.deca ; do
         file="${f%.deca}"
         ((nbtests++))
+        # if $2 is set, we want to test the codegen with the given compiler
+        if [ "$2" != "" ]; then
+            decac -p "${f}" > "${file}_decompiled.deca"
+        fi
         if [ -f "${file}.in" ]; then
+            if [ "$2" != "" ]; then
+            decac "${file}_decompiled.deca" && (cat "${file}.in" | ima "${file}_decompiled.ass" > "${file}_decompiled.res")
+            fi
             decac "${f}" && (cat "${file}.in" | ima "${file}.ass" > "${file}.res")
         else
+            if [ "$2" != "" ]; then
+                decac "${file}_decompiled.deca" && (ima "${file}_decompiled.ass" > "${file}_decompiled.res")
+            fi
             decac "${f}" && (ima "${file}.ass" > "${file}.res")
         fi
         if [ -f "${file}.res" ]; then
-    	if diff -q "${file}.res" "${file}.expected" > /dev/null ; then
-    	    echo "--- ${file}: PASSED ---"
-    	    ((nbpassed++))
-    	else
-    	    echo "--- ${file}: FAILED ---"
-    	    diff "${file}.expected" "${file}.res" 
-    	fi
+    	    if diff -q "${file}.res" "${file}.expected" > /dev/null ; then
+                if [ "$2" != "" ]; then
+                    if [ -f "${file}_decompiled.res" ]; then
+    	                if diff -q "${file}_decompiled.res" "${file}.expected" > /dev/null ; then
+                            echo "--- ${file}: PASSED ---"
+    	                    ((nbpassed++))
+                        else
+                            echo "--- ${file}: FAILED ---"
+                            diff "${file}.expected" "${file}_decompiled.res"
+                        fi
+                    else
+                        echo "--- ${file}: KO ---"
+                    fi
+                else
+    	            echo "--- ${file}: PASSED ---"
+    	            ((nbpassed++))
+                fi
+    	    else
+    	        echo "--- ${file}: FAILED ---"
+    	        diff "${file}.expected" "${file}.res" 
+    	    fi
         else
-    	echo "--- ${file}: KO ---"
+    	    echo "--- ${file}: KO ---"
         fi
         echo
     done
