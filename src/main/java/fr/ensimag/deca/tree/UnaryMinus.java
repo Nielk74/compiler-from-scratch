3554fc2ec64b45c0e324bbc32c409010aecdeead
Tien Dat Phan <tien-dat.phan@grenoble-inp.org>
2022-06-10 16:28:28 +0000

    Resolve "unary minus"

--- src/main/java/fr/ensimag/deca/tree/UnaryMinus.java
+++ src/main/java/fr/ensimag/deca/tree/UnaryMinus.java
@@ -19,13 +19,31 @@ public class UnaryMinus extends AbstractUnaryExpr {
     @Override
     public Type verifyExpr(DecacCompiler compiler, EnvironmentExp localEnv,
             ClassDefinition currentClass) throws ContextualError {
-        throw new UnsupportedOperationException("not yet implemented");
+        Type typeOperand = getOperand().verifyExpr(compiler, localEnv, currentClass);
+        if (!typeOperand.isFloat() && !typeOperand.isInt()){
+            throw new ContextualError("Unary minus cannot be applied to a value other than float or int ", getLocation());
+        }
+        setType(typeOperand);
+        return typeOperand;
     }
 
 
     @Override
     protected String getOperatorName() {
         return "-";
+    }
+
+    @Override
+    protected void codeGenExp(DecacCompiler compiler, int register_name) {
+        Minus minusObject;
+        if (getOperand().getType().isInt()){
+            minusObject= new Minus(new IntLiteral(0), getOperand());
+        }
+        else {
+            minusObject= new Minus(new FloatLiteral(0), getOperand());
+        }
+        
+        minusObject.codeGenExp(compiler, register_name);
     }
 
 }
